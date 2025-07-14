e2ee73df67f7d08bfb1c3328175e0af8d2c9c3f7
Volodymyr Tkach <vovantkac5@gmail.com>
2025-07-14 09:51:42 +0300

    Revert "Update api.js"
    This reverts commit 3fac2e0facc1d294ca01d220e4ca377585e99f6e.

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // config/api.js
 export const API_CONFIG = {
-    BASE_URL: "https://robocorp-telegram-be.datafabdevelopment.com",
+    BASE_URL: "https://maximum-allowing-wildcat.ngrok-free.app",
     SKIP_BROWSER_WARNING: "69420"
 };
 
