6636134fe297baa7b964538e0ce62da610fe0f06
Artem Hirzhev <a.hirzhev@gmail.com>
2025-07-24 14:20:36 +0300

    Remove excess code from api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,8 @@
-import {Source} from "@/src/types";
+import { Source } from "@/src/types";
 
 export const API_CONFIG = {
   BASE_URL: "https://robocorp-telegram-be.datafabdevelopment.com",
-  SKIP_BROWSER_WARNING: "69420"
+  SKIP_BROWSER_WARNING: "69420",
 };
 
 export const BOT_CONFIG = {
@@ -11,12 +11,41 @@ export const BOT_CONFIG = {
 };
 
 export const SOURCES_DATA: Source[] = [
-  {key: "youtubeConnected", name: "YouTube", icon: "fab fa-youtube", color: "#FF0000", disabled: false},
-  {key: "spotifyConnected", name: "Spotify", icon: "fab fa-spotify", color: "#1DB954", disabled: false},
-  {key: "redditConnected", name: "Reddit", icon: "fab fa-reddit", color: "#FF4500", disabled: false},
-  {key: "xConnected", name: "X", icon: "fab fa-x-twitter", color: "#000000", disabled: false},
-  {key: "walletConnected", name: "Wallet", icon: "fa-solid fa-wallet", color: "#4e44ce", disabled: false},
-  // {key: "linkedinConnected", name: "LinkedIn", icon: "fab fa-linkedin", color: "#0077B5", disabled: true}
+  {
+    key: "youtubeConnected",
+    name: "YouTube",
+    icon: "fab fa-youtube",
+    color: "#FF0000",
+    disabled: false,
+  },
+  {
+    key: "spotifyConnected",
+    name: "Spotify",
+    icon: "fab fa-spotify",
+    color: "#1DB954",
+    disabled: false,
+  },
+  {
+    key: "redditConnected",
+    name: "Reddit",
+    icon: "fab fa-reddit",
+    color: "#FF4500",
+    disabled: false,
+  },
+  {
+    key: "xConnected",
+    name: "X",
+    icon: "fab fa-x-twitter",
+    color: "#000000",
+    disabled: false,
+  },
+  {
+    key: "walletConnected",
+    name: "Wallet",
+    icon: "fa-solid fa-wallet",
+    color: "#4e44ce",
+    disabled: false,
+  },
 ];
 
 export const PLATFORM_OPTIONS = [
@@ -26,9 +55,17 @@ export const PLATFORM_OPTIONS = [
     icon: "fab fa-youtube",
     color: "#FF0000",
     types: [
-      {label: "Liked Videos", value: "yt_liked_videos", icon: "fas fa-thumbs-up"},
-      {label: "Subscriptions", value: "yt_subscriptions", icon: "fas fa-bell"}
-    ]
+      {
+        label: "Liked Videos",
+        value: "yt_liked_videos",
+        icon: "fas fa-thumbs-up",
+      },
+      {
+        label: "Subscriptions",
+        value: "yt_subscriptions",
+        icon: "fas fa-bell",
+      },
+    ],
   },
   {
     platform: "Spotify",
@@ -36,8 +73,12 @@ export const PLATFORM_OPTIONS = [
     icon: "fab fa-spotify",
     color: "#1DB954",
     types: [
-      {label: "Recently Played", value: "spotify_recently_played_tracks", icon: "fas fa-history"}
-    ]
+      {
+        label: "Recently Played",
+        value: "spotify_recently_played_tracks",
+        icon: "fas fa-history",
+      },
+    ],
   },
   {
     platform: "IP Geo Data",
@@ -45,8 +86,8 @@ export const PLATFORM_OPTIONS = [
     icon: "fab fa-globe",
     color: "#3554e3",
     types: [
-      {label: "My Geo Data", value: "user_geo_data", icon: "fas fa-history"}
-    ]
+      { label: "My Geo Data", value: "user_geo_data", icon: "fas fa-history" },
+    ],
   },
   {
     platform: "Reddit",
@@ -54,11 +95,15 @@ export const PLATFORM_OPTIONS = [
     icon: "fab fa-reddit",
     color: "#FF4500",
     types: [
-      {label: "Upvoted posts", value: "upvoted", icon: "fas fa-thumbs-up"},
-      {label: "Downvoted posts", value: "downvoted", icon: "fas fa-thumbs-down"},
-      {label: "Saved posts", value: "saved", icon: "fas fa-bookmark"},
-      {label: "Submitted posts", value: "submitted", icon: "fas fa-upload"},
-    ]
+      { label: "Upvoted posts", value: "upvoted", icon: "fas fa-thumbs-up" },
+      {
+        label: "Downvoted posts",
+        value: "downvoted",
+        icon: "fas fa-thumbs-down",
+      },
+      { label: "Saved posts", value: "saved", icon: "fas fa-bookmark" },
+      { label: "Submitted posts", value: "submitted", icon: "fas fa-upload" },
+    ],
   },
   {
     platform: "X",
@@ -66,8 +111,16 @@ export const PLATFORM_OPTIONS = [
     icon: "fab fa-x-twitter",
     color: "#FF4500",
     types: [
-      {label: "Liked posts", value: "x_liked_posts", icon: "fas fa-thumbs-up"},
-      {label: "Published posts", value: "x_published_posts", icon: "fas fa-upload"},
-    ]
+      {
+        label: "Liked posts",
+        value: "x_liked_posts",
+        icon: "fas fa-thumbs-up",
+      },
+      {
+        label: "Published posts",
+        value: "x_published_posts",
+        icon: "fas fa-upload",
+      },
+    ],
   },
 ];
