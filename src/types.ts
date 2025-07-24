519fdc1939669fd1276c5df6ad39488b3b106fba
Artem Hirzhev <a.hirzhev@gmail.com>
2025-07-24 12:07:29 +0300

    Add type to PlatformSelector

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,17 @@ export type Tab = {
   icon: string;
   component: () => React.JSX.Element;
 };
+
+export type PlatformType = {
+  label: string;
+  value: string;
+  icon: string;
+};
+
+export type Platform = {
+  platform: string;
+  value: string;
+  icon: string;
+  color: string;
+  types: PlatformType[];
+};
